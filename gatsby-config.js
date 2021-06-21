9506ce813ead300a6354592846674d7ce7a75896
Kyle Soeltz <wsoeltz@gmail.com>
2021-06-21 12:34:13 -0400

    Remove gatsby-plugin-sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
   plugins: [
     "gatsby-plugin-styled-components",
     "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-sitemap",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
